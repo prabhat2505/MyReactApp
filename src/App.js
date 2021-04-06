b202e28956619e4a62f6c8d3ac2b21f17a003583
Prabhat <prabhat.rana828@gmail.com>
2021-04-06 19:14:56 +0530

    Adding two number

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BasicForm from './Forms/BasicForm';
 import ExampleHook1 from './Forms/ExampleHook1';
 import ExampleUseRef1 from './Forms/ExampleUseRef1';
 import ExampleUseRef2 from './Forms/ExampleUseRef2';
+import Calculator from './Forms/Calculator';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
     {/* <BasicForm></BasicForm>    */}
     {/* <ExampleHook1></ExampleHook1>  */}
     {/* <ExampleUseRef1></ExampleUseRef1> */}
-    <ExampleUseRef2></ExampleUseRef2>
+    {/* <ExampleUseRef2></ExampleUseRef2> */}
+     <Calculator></Calculator>
 
     </div>
   );
