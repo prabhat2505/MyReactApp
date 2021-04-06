6a83ecd0815a1a9fd11f152a42760fd270144a5c
Prabhat <prabhat.rana828@gmail.com>
2021-04-06 16:31:46 +0530

    use of usesate for counting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import BasicForm from './Forms/BasicForm'
+import BasicForm from './Forms/BasicForm';
+import ExampleHook1 from './Forms/ExampleHook1';
 
 function App() {
   return (
     <div className="App">
-    <BasicForm></BasicForm>   
+    {/* <BasicForm></BasicForm>    */}
+    <ExampleHook1></ExampleHook1> 
 
     </div>
   );
