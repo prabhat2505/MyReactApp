bbd4a424d433e9ab5cd2fdd476250c6125e0a19c
Prabhat <prabhat.rana828@gmail.com>
2021-04-06 17:37:09 +0530

    Inputfocus on click

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import BasicForm from './Forms/BasicForm';
 import ExampleHook1 from './Forms/ExampleHook1';
+import ExampleUseRef1 from './Forms/ExampleUseRef1';
 
 function App() {
   return (
     <div className="App">
     {/* <BasicForm></BasicForm>    */}
-    <ExampleHook1></ExampleHook1> 
+    {/* <ExampleHook1></ExampleHook1>  */}
+    <ExampleUseRef1></ExampleUseRef1>
 
     </div>
   );
---
+++ src/Forms/ExampleUseRef1.js
@@ -0,0 +1,15 @@
+import React, { useRef } from 'react';
+function ExampleUseRef1() {
+    const inputEl = useRef(null);
+    const onButtonClick = () => {
+      // `current` points to the mounted text input element
+      inputEl.current.focus();
+    };
+    return (
+      <>
+        <input ref={inputEl} type="text" />
+        <button onClick={onButtonClick}>Focus the input</button>
+      </>
+    );
+  }
+  export default ExampleUseRef1;
