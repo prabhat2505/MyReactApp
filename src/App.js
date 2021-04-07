ed4c1b1ce03a7c442bb6567c30e9a2426342a2bd
Prabhat <prabhat.rana828@gmail.com>
2021-04-07 15:18:51 +0530

    Use of array map example 1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ExampleUseRef2 from './Forms/ExampleUseRef2';
 import Calculator from './Forms/Calculator';
 import Datafetch1 from './Forms/Datafetch1';
 import ExampleUseReducer1 from './Forms/ExampleUseReducer1';
+import ArrayMap from './Forms/ArrayMap';
 
 function App() {
   return (
@@ -17,7 +18,8 @@ function App() {
     {/* <ExampleUseRef2></ExampleUseRef2> */}
      {/* <Calculator></Calculator> */}
      {/* <Datafetch1></Datafetch1> */}
-     <ExampleUseReducer1></ExampleUseReducer1>
+     {/* <ExampleUseReducer1></ExampleUseReducer1> */}
+     <ArrayMap></ArrayMap>
      
 
     </div>
