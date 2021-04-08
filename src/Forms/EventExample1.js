788233893f861b03c648e7aa728d33aad5f99b52
Prabhat <prabhat.rana828@gmail.com>
2021-04-08 17:25:45 +0530

    Event with this object

---
+++ src/Forms/EventExample1.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+class EventExample1 extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {isToggleOn: true};
+  
+      // This binding is necessary to make `this` work in the callback
+      //this.handleClick = this.handleClick.bind(this);
+    }
+  
+    // handleClick() {
+    //   this.setState(prevState => ({
+    //     isToggleOn: !prevState.isToggleOn
+    //   }));
+    // }
+  
+     // This syntax ensures `this` is bound within handleClick.
+  // Warning: this is *experimental* syntax.
+    handleClick = () => {
+        this.setState(prevState => ({
+                isToggleOn: !prevState.isToggleOn
+               }));
+    }
+
+    render() {
+      return (
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+      );
+    }
+  }
+  export default EventExample1;
