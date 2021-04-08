ad8ad6330df7be962bd367266e19ae47f728a06e
Prabhat <prabhat.rana828@gmail.com>
2021-04-08 11:40:54 +0530

    Ussing Of css example 1

---
+++ src/Forms/Stylesheet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import  './Mystyle.css'
+
+function Stylesheet(){
+
+    return(
+        <div>
+            <h1 className='primary'>Hello</h1>
+        </div>
+    )
+}
+export default Stylesheet;
