8e3b59ce7763c3df01d1eec90d4a07d04ba6582b
Prabhat <prabhat.rana828@gmail.com>
2021-04-08 11:56:15 +0530

    using css with Module

--- src/Forms/Stylesheet.js
+++ src/Forms/Stylesheet.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import  './Mystyle.css'
+import  style from './Customstyle.module.css'
 
 function Stylesheet(){
 
     return(
         <div>
             <h1 className='primary'>Hello</h1>
+            <h1 className={style.sucess}>Hello</h1>
         </div>
     )
 }
