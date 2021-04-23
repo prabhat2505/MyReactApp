8a388156544bd08978a87aa988bf0c1d8e7420a7
Prabhat <prabhat.rana828@gmail.com>
2021-04-23 17:49:52 +0530

    Dyamic Inline css:--Togling colour with event

--- src/SyleType/inline.js
+++ src/SyleType/inline.js
@@ -2,11 +2,23 @@ import React, { Component, useState } from 'react';
 function Inline() {
 
 const [isValid,setIsValid]=useState(false);
-     
+const changeColorTogle= ()=>{
+    if(!isValid)
+    {
+        setIsValid(true)
+    }
+    else {
+        setIsValid(false)
+    }
+    
+}
     return (
+        <div>
       <label style={{color:!isValid ? 'red':'black'}}>
         Click me
       </label>
+      <button onClick={changeColorTogle}>Change</button>
+      </div>
     );
   }
   export default Inline;
