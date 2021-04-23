0bb1fcac2bc9e58372b974c7a718c8559a7ea5a6
Prabhat <prabhat.rana828@gmail.com>
2021-04-23 23:19:13 +0530

    css-with-props

---
+++ src/SyleType/css-with-props.js
@@ -0,0 +1,22 @@
+import React, { Component, useState } from 'react';
+import styled from 'styled-components';
+
+const FormControl=styled.div`
+
+& input{
+    border:1px solid ${props=>(props.invalid ? 'red' : '#ccc')};
+}
+`;
+function CssWithProps() {
+
+const [isValid,setIsValid]=useState(false);
+
+    return (
+        <div>
+        <FormControl invalid={!isValid}>
+        <input type="text"></input>
+        </FormControl>
+      </div>
+    );
+  }
+  export default CssWithProps;
