6337daccba748459de1a9466f1ecca72270b8bb7
Prabhat <prabhat.rana828@gmail.com>
2021-06-07 14:51:41 +0530

    Adding a layout wrapper component

--- src/App.js
+++ src/App.js
@@ -20,17 +20,11 @@ import './App.css';
 // import Datafetch2 from './Forms/Datafetch2';
 // import BasicExample from './RouterBasic.js/BasicExample';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  BrowserRouter
-} from "react-router-dom";
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import MainHeader from './components/MainHeader';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+import Layout from './components/layout/Layout';
 // import Updatelist from './Forms/Updatelist';
 // import styled from 'styled-components';
 
@@ -38,7 +32,7 @@ import MainHeader from './components/MainHeader';
 // import Inline from './SyleType/inline';
 // import DynamicsClass from './SyleType/dynamics-class';
 // import CssWithProps from './SyleType/css-with-props';
-import Getadata from './fetch/getdata';
+//import Getadata from './fetch/getdata';
 
 // const Flex = styled.div`
 //   display: flex;
@@ -53,22 +47,22 @@ import Getadata from './fetch/getdata';
 
 function App() {
   return (
-    <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path='/welcome'>
-            <Welcome />
-          </Route>
-          <Route path='/products' exact>
-            <Products />
-          </Route>
-          <Route path='/products/:productId'>
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
-    </div>
+    <Layout>
+    <Switch>
+      <Route path='/' exact>
+        <Redirect to='/quotes' />
+      </Route>
+      <Route path='/quotes' exact>
+        <AllQuotes />
+      </Route>
+      <Route path='/quotes/:quoteId'>
+        <QuoteDetail />
+      </Route>
+      <Route path='/new-quote'>
+        <NewQuote />
+      </Route>
+    </Switch>
+  </Layout>
   );
 }
 
