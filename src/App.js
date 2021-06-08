bd5d6e757507b484fb15ad1f2f526f2ed6dc1667
Prabhat <prabhat.rana828@gmail.com>
2021-06-08 11:42:37 +0530

    Fetching using dummu data

--- src/App.js
+++ src/App.js
@@ -1,68 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
-// import BasicForm from './Forms/BasicForm';
-// import ExampleHook1 from './Forms/ExampleHook1';
-// import ExampleUseRef1 from './Forms/ExampleUseRef1';
-// import ExampleUseRef2 from './Forms/ExampleUseRef2';
-// import Calculator from './Forms/Calculator';
-// import Datafetch1 from './Forms/Datafetch1';
-// import ExampleUseReducer1 from './Forms/ExampleUseReducer1';
-// import ArrayMap from './Forms/ArrayMap';
-// import GetDataInSateMap from './Forms/GetaDataInSateMap';
-// import HorizontalLinearStepper from './Forms/HorizontalLinearStepper';
-// import Registartion from './Forms/Registartion';
-// import Stylesheet from './Forms/Stylesheet';
-// import Register from './Forms/RegisterClass';
-// import ActionLink from './Forms/ActionLink';
-// import EventExample1 from './Forms/EventExample1';
-// import FormValidation from './Forms/FormValidation';
-// import { ForwardRefParent } from './Forms/ForwardRefParent';
-// import Datafetch2 from './Forms/Datafetch2';
-// import BasicExample from './RouterBasic.js/BasicExample';
-
-import { Route, Switch, Redirect } from 'react-router-dom';
-import AllQuotes from './pages/AllQuotes';
-import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
-import Layout from './components/layout/Layout';
-// import Updatelist from './Forms/Updatelist';
-// import styled from 'styled-components';
-
-// import ReduxBasics from './Redux/ReduxBasics';
-// import Inline from './SyleType/inline';
-// import DynamicsClass from './SyleType/dynamics-class';
-// import CssWithProps from './SyleType/css-with-props';
-//import Getadata from './fetch/getdata';
-
-// const Flex = styled.div`
-//   display: flex;
-
-//   @media (max-width: 1006px) {
-//     flex-direction: column;
-//   }
-//   @media (max-height: 1006px) {
-//     flex-direction: column;
-//   }
-// `;
 
 function App() {
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
+
   return (
-    <Layout>
-    <Switch>
-      <Route path='/' exact>
-        <Redirect to='/quotes' />
-      </Route>
-      <Route path='/quotes' exact>
-        <AllQuotes />
-      </Route>
-      <Route path='/quotes/:quoteId'>
-        <QuoteDetail />
-      </Route>
-      <Route path='/new-quote'>
-        <NewQuote />
-      </Route>
-    </Switch>
-  </Layout>
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
   );
 }
 
